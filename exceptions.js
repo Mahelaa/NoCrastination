7d0468e03e7ad3aa58c305562b4c4beda3a1777c
Mahela Abeysekera <mahela32@gmail.com>
2017-04-11 09:45:20 +0530

    Add files via upload

---
+++ exceptions.js
@@ -0,0 +1,13 @@
+var siteslist = ["www.youtube.com"];
+var timeout = setTimeout(function() {}, 10);
+
+
+do while (oremium!= null){
+
+
+console.log("Enter site list to be blocked : ");
+prompt (" Sites :");
+
+}
+
+document.getElementById("clickMe").onclick = doFunction;
